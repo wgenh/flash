f51249fed31f4efe1a397c7ae40df69b6328109a
wgenh <wgenh@users.noreply.github.com>
2024-06-03 19:35:53 +0300

    pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { svelteTesting } from '@testing-library/svelte/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
 	root: "src",
+	base: "flash",
 	build: {
 		outDir: "../out",
 		emptyOutDir: true
